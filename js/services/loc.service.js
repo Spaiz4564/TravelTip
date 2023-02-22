15b3c7bf54dd4faa438a7fa62892b3df039e5197
ylcN101 <yuvalcohen12234@gmail.com>
2023-02-22 17:23:22 +0200

    add

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,23 +1,53 @@
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+
 export const locService = {
   getLocs,
+  getLocsForDisplay,
 }
 
-// 4. Build the placeService managing Places:
-// {id, name, lat, lng, weather, createdAt, updatedAt}
-// a. NOTE: this service should work with the async-storage.service
-// 5. Render the places table:
-// Locations Table
-// a. Show the place information
-
-const locs = [
-  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
-]
+_createLocs()
 
 function getLocs() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(locs)
-    }, 2000)
-  })
+  return storageService.query('locsDB')
+}
+
+function _createLocs() {
+  const locs = utilService.loadFromStorage('locsDB')
+  if (!locs || !locs.length) {
+    _createDemoLocs()
+  }
+}
+
+function _createLoc(name) {
+  const loc = getEmptyLoc()
+  loc.name = name || utilService.makeLorem(10)
+  loc.lat = utilService.getRandomIntInclusive(29.5, 32.5)
+  loc.lng = utilService.getRandomIntInclusive(34.5, 36.5)
+  loc.createdAt = Date.now()
+  loc.updatedAt = Date.now()
+  return loc
+}
+
+function getEmptyLoc() {
+  return {
+    id: utilService.makeId(),
+    name: '',
+    lat: 0,
+    lng: 0,
+    weather: '',
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  }
+}
+
+function _createDemoLocs() {
+  const locNames = ['Tel Aviv', 'Munich', 'Yavne']
+  const locs = locNames.map((name) => _createLoc(name))
+  utilService.saveToStorage('locsDB', locs)
+  return locs
+}
+
+function getLocsForDisplay() {
+  return getLocs()
 }
